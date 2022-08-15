ce228cde5f272a991136ab39d0a6f6160d25ae34
mstach0wicz <mikolaj.stachowicz0@gmail.com>
2022-08-15 19:35:50 +0200

    Add new HtmlWebpackPlugin to plugins

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,15 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   entry: {
     index: './src/index.js',
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+        title: 'Restaurant page',
+    })
+  ],
  devServer: {
    static: './dist',
  },
