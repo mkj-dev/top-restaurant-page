2223c9cbaa4ac9a44b4460b3845b16942129552e
mstach0wicz <mikolaj.stachowicz0@gmail.com>
2022-10-01 16:47:27 +0200

    Delete old project configuration files

--- src/index.js
+++
@@ -1,9 +0,0 @@
-function component() {
-    const element = document.createElement('div');
-  
-    element.innerText = 'Test message...';
-
-    return element;
-  }
-  
-  document.body.appendChild(component());
