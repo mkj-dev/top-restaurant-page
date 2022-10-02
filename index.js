faac1b58929b4579cc257d934733fa8a9dd49da4
mstach0wicz <mikolaj.stachowicz0@gmail.com>
2022-10-02 18:54:09 +0200

    Move code to create the header and buttons to header.js

--- index.js
+++ index.js
@@ -1,23 +1,6 @@
+import {header, h2, makeButtons} from "./header.js"
+
 const content = document.getElementById('content')
-const header = document.createElement('header')
-const h2 = document.createElement('h2')
+content.appendChild(header, h2)
 
-header.setAttribute('id', 'header')
-content.appendChild(header)
-header.appendChild(h2)
-h2.innerText = 'Lorem Ipsum Restaurant'
-
-for (let i = 1; i < 4; i++) {
-    const pageBtn = document.createElement('button')
-    pageBtn.setAttribute('id', 'pageButton' + i)
-    
-    if (pageBtn.id === 'pageButton1') {
-        pageBtn.innerText = 'Home'
-    } else if (pageBtn.id === 'pageButton2') {
-        pageBtn.innerText = 'Menu'
-    } else if (pageBtn.id === 'pageButton3') {
-        pageBtn.innerText = 'Contact'
-    }
-
-    header.appendChild(pageBtn)
-}
+makeButtons()
