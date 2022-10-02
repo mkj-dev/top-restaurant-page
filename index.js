2b8da779078b8fcec4244e05476a40754bb2fe63
mstach0wicz <mikolaj.stachowicz0@gmail.com>
2022-10-02 21:07:28 +0200

    Import footer from footer.js

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 import {header, h2, makeButtons} from "./header.js"
+import {footer, p} from "./footer.js"
 
 const content = document.getElementById('content')
 content.appendChild(header, h2)
+content.appendChild(footer, p)
 
 makeButtons()
