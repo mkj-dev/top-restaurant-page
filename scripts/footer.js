6f6298f9008c092729e1ba316fa948ec4081f9fb
mstach0wicz <mikolaj.stachowicz0@gmail.com>
2022-10-03 16:46:20 +0200

    Change content of the footer

--- scripts/footer.js
+++ scripts/footer.js
@@ -2,7 +2,7 @@ const footer = document.createElement('footer')
 const p = document.createElement('p')
 
 footer.setAttribute('id', 'footer')
-p.innerText = 'Created by mkj-dev for TheOdinProject "Restaurant Page" 2022'
+p.innerText = '\u00A9 2022 github.com/mkj-dev'
 footer.appendChild(p)
 
 export {footer, p}
