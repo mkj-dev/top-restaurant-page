4130e6263924469dea14848f42fe2abbc143a2bd
mstach0wicz <mikolaj.stachowicz0@gmail.com>
2022-10-04 00:54:46 +0200

    Import gallery from home.js

--- scripts/index.js
+++ scripts/index.js
@@ -1,12 +1,13 @@
 import {header, h2, makeButtons} from "./header.js"
 import {footer, p} from "./footer.js"
-import {homeSection, backgroundImage, aboutUs, makeImages} from "./home.js"
+import {homeSection, backgroundImage, aboutUs, gallery, makeImages} from "./home.js"
 
 const content = document.getElementById('content')
 
 content.appendChild(header, h2)
 content.appendChild(homeSection, backgroundImage)
 content.appendChild(aboutUs)
+content.appendChild(gallery)
 content.appendChild(footer, p)
 
 makeButtons()
