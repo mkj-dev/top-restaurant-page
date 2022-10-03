8e8aa5791a38513aca9e3b5a70e014af616443e5
mstach0wicz <mikolaj.stachowicz0@gmail.com>
2022-10-03 18:42:45 +0200

    Import home.js

--- scripts/index.js
+++ scripts/index.js
@@ -1,8 +1,10 @@
 import {header, h2, makeButtons} from "./header.js"
 import {footer, p} from "./footer.js"
+import {div as homeSection, backgroundImage} from "./home.js"
 
 const content = document.getElementById('content')
 content.appendChild(header, h2)
+content.appendChild(homeSection, backgroundImage)
 content.appendChild(footer, p)
 
 makeButtons()
