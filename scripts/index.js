00e8a261d19ebd4ffa8df1db993849f2dcb4e7d0
mstach0wicz <mikolaj.stachowicz0@gmail.com>
2022-10-07 13:53:56 +0200

    Move menu.js and contact.js imports to header.js

--- scripts/index.js
+++ scripts/index.js
@@ -1,8 +1,6 @@
 import { header, h2, createButtons } from "./header.js"
 import { footer, p } from "./footer.js"
 import { homeSection, backgroundImage, aboutUs, gallery, createImages } from "./home.js"
-import  menuSection  from "./menu.js"
-import contactSection from "./contact.js"
 
 const content = document.getElementById('content')
 
@@ -10,8 +8,6 @@ content.appendChild(header, h2)
 content.appendChild(homeSection, backgroundImage)
 content.appendChild(aboutUs)
 content.appendChild(gallery)
-content.appendChild(menuSection)
-content.appendChild(contactSection)
 content.appendChild(footer, p)
 
 createButtons()
