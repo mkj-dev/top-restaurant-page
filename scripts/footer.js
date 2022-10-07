e5f19ce23046ae8b18d043df274a069c6e07ccc0
mstach0wicz <mikolaj.stachowicz0@gmail.com>
2022-10-07 14:39:16 +0200

    Add source for used images

--- scripts/footer.js
+++ scripts/footer.js
@@ -1,8 +1,14 @@
 const footer = document.createElement('footer')
 const p = document.createElement('p')
+const source = document.createElement('a')
 
 footer.setAttribute('id', 'footer')
 p.innerText = '\u00A9 2022 github.com/mkj-dev'
-footer.appendChild(p)
 
-export {footer, p}
+source.href = 'https://www.freepik.com/author/valeria-aksakova'
+source.innerText = 'Images by valeria_aksakova on Freepik'
+
+footer.appendChild(p)
+footer.appendChild(source)
+
+export { footer }
