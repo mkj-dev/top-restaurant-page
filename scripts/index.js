c28cce4ec65c62cbd247263cc0e549422f3010c1
mstach0wicz <mikolaj.stachowicz0@gmail.com>
2022-10-07 14:42:34 +0200

    Replace appendChild() with after() on footer

--- scripts/index.js
+++ scripts/index.js
@@ -1,5 +1,5 @@
 import { header, h2, createButtons } from "./header.js"
-import { footer, p } from "./footer.js"
+import { footer } from "./footer.js"
 import { homeSection, backgroundImage, aboutUs, gallery, createImages } from "./home.js"
 
 const content = document.getElementById('content')
@@ -8,7 +8,7 @@ content.appendChild(header, h2)
 content.appendChild(homeSection, backgroundImage)
 content.appendChild(aboutUs)
 content.appendChild(gallery)
-content.appendChild(footer, p)
+content.after(footer)
 
 createButtons()
 createImages()
