5dce844534aa2bdd1c11ea7d489eb32a43574e38
mstach0wicz <mikolaj.stachowicz0@gmail.com>
2023-02-08 14:01:39 +0100

    Code refactoring - index code more concise, tidier

--- index.js
+++ index.js
@@ -1,14 +1,9 @@
-import { header, h2, createButtons } from "./modules/header.js"
+import { header } from "./modules/header.js"
+import { homeSection, createImages } from "./modules/home.js"
 import { footer } from "./modules/footer.js"
-import { homeSection, backgroundImage, aboutUs, gallery, createImages } from "./modules/home.js"
 
 const content = document.getElementById('content')
 
-content.appendChild(header, h2)
-content.appendChild(homeSection, backgroundImage)
-content.appendChild(aboutUs)
-content.appendChild(gallery)
-content.after(footer)
-
-createButtons()
+content.append(header, homeSection)
 createImages()
+content.after(footer)
