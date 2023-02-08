c4e3ca1382ed819085a5add85db3aff18cc1b569
mstach0wicz <mikolaj.stachowicz0@gmail.com>
2023-02-08 17:15:42 +0100

    Change the menu array to an object

--- modules/menu.js
+++ modules/menu.js
@@ -11,13 +11,21 @@ menuHeading.innerText = 'Menu'
 const menuElements = document.createElement('div')
 menuElements.setAttribute('id', 'menuElements')
 
-const menu = ['Shrimp Fra Diavolo', 'Baked Clams', 'Fried Calamari', 'Lobster', 'Caesar Salad', 'Meatballs']
+const menu = {
+    'The Classic': 7.99,
+    'The Cheeseburge Deluxe': 8.99,
+    'The Baconator': 10.99,
+    'The Mushroom Swiss': 9.99,
+    'The BBQ Burger': 8.99,
+    'The Cowboy Burger': 10.99,
+    'The Spicy Jalapeno Burger': 9.99
+}
 
-menu.forEach(element => {
+for (const burger in menu) {
     const h3 = document.createElement('h3')
-    h3.innerText = `${element}.........................$20.50`
+    h3.innerText = `${burger}: $${menu[burger]}`
     menuElements.appendChild(h3)
-})
+}
 
 menuSection.append(menuHeading, menuElements)
 
