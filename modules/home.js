323e9a151436c46f15272b78d6f15a939177bf9c
mstach0wicz <mikolaj.stachowicz0@gmail.com>
2023-02-08 14:00:03 +0100

    Code refactoring - home code more concise, tidier

--- modules/home.js
+++ modules/home.js
@@ -1,21 +1,28 @@
-const homeSection = document.createElement('div')
-const backgroundImage = document.createElement('img')
-const aboutUs = document.createElement('div')
-const gallery = document.createElement('div')
-const h3About = document.createElement('h3')
-const p = document.createElement('p')
-const h3Gallery = document.createElement('h3')
-
+// Home section
+const homeSection = document.createElement('section')
 homeSection.setAttribute('id', 'homeSection')
-backgroundImage.setAttribute('id', 'homeSectionBgImg')
+
+// About us
+const aboutUs = document.createElement('div')
 aboutUs.setAttribute('id', 'aboutUs')
+
+// About us heading
+const aboutUsHeading = document.createElement('h3')
+aboutUsHeading.innerText = 'About Us'
+
+// About us paragraph
+const aboutPara = document.createElement('p')
+aboutPara.innerText = 'Aenean consequat suscipit lorem non rutrum. Nullam consequat ac augue at tincidunt. Suspendisse nec consectetur sapien. Vestibulum faucibus, est a commodo gravida, nisl tellus euismod dolor, non condimentum quam diam a nulla. Praesent nec nibh at enim imperdiet hendrerit. Pellentesque sit amet feugiat sapien. Aliquam egestas fermentum auctor. Nulla vel erat dolor. Nulla porttitor tellus urna, malesuada sodales augue fermentum eget. Ut eu lobortis nisl, sit amet consectetur ipsum. Sed hendrerit diam enim, id ultricies sem aliquam et.'
+
+// Gallery
+const gallery = document.createElement('div')
 gallery.setAttribute('id', 'gallery')
 
-h3About.innerText = 'About Us'
-p.innerText = 'Aenean consequat suscipit lorem non rutrum. Nullam consequat ac augue at tincidunt. Suspendisse nec consectetur sapien. Vestibulum faucibus, est a commodo gravida, nisl tellus euismod dolor, non condimentum quam diam a nulla. Praesent nec nibh at enim imperdiet hendrerit. Pellentesque sit amet feugiat sapien. Aliquam egestas fermentum auctor. Nulla vel erat dolor. Nulla porttitor tellus urna, malesuada sodales augue fermentum eget. Ut eu lobortis nisl, sit amet consectetur ipsum. Sed hendrerit diam enim, id ultricies sem aliquam et.'
-backgroundImage.src = '../../images/vegetables-set-left-black-slate.jpg'
-h3Gallery.innerText = 'Gallery'
+// Gallery heading
+const galleryHeading = document.createElement('h3')
+galleryHeading.innerText = 'Gallery'
 
+// Gallery images
 function createImages() {
     for (let i = 0; i < 6; i++) {
         const img = document.createElement('img')
@@ -26,7 +33,9 @@ function createImages() {
     }
 }
 
-aboutUs.appendChild(h3About), aboutUs.appendChild(p), gallery.appendChild(h3Gallery)
-homeSection.appendChild(backgroundImage, aboutUs, h3Gallery)
+aboutUs.append(aboutUsHeading, aboutPara)
+gallery.appendChild(galleryHeading)
 
-export { homeSection, backgroundImage, aboutUs, gallery, h3Gallery, createImages }
+homeSection.append(aboutUs, gallery)
+
+export { homeSection, createImages }
