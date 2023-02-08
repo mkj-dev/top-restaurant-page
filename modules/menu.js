7d2b817482d7d5bc68d0d167b584735e0e60e807
mstach0wicz <mikolaj.stachowicz0@gmail.com>
2023-02-08 14:00:19 +0100

    Code refactoring - menu code more concise, tidier

--- modules/menu.js
+++ modules/menu.js
@@ -1,20 +1,24 @@
-const menuSection = document.createElement('div')
+// Menu section
+const menuSection = document.createElement('section')
+menuSection.setAttribute('id', 'menuSection')
+
+// Menu heading
 const menuHeading = document.createElement('h2')
-const menuDish = document.createElement('div')
+menuHeading.setAttribute('id', 'menuHeading')
+menuHeading.innerText = 'Menu'
+
+// Menu elements
+const menuElements = document.createElement('div')
+menuElements.setAttribute('id', 'menuElements')
 
 const menu = ['Shrimp Fra Diavolo', 'Baked Clams', 'Fried Calamari', 'Lobster', 'Caesar Salad', 'Meatballs']
 
-menuSection.setAttribute('id', 'menuSection')
-menuDish.setAttribute('id', 'menuItems')
-
-menuHeading.innerText = 'Menu'
-menu.forEach(dish => {
-    let h3 = document.createElement('h3')
-    h3.innerText = `${dish}.........................$20.50`
-    menuDish.appendChild(h3)
+menu.forEach(element => {
+    const h3 = document.createElement('h3')
+    h3.innerText = `${element}.........................$20.50`
+    menuElements.appendChild(h3)
 })
 
-menuSection.appendChild(menuHeading)
-menuSection.appendChild(menuDish)
+menuSection.append(menuHeading, menuElements)
 
-export { menuSection, menuDish }
+export { menuSection }
