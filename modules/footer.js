cd4b2a783d214d004c4a3f402b230fda761fc72c
mstach0wicz <mikolaj.stachowicz0@gmail.com>
2023-02-09 06:51:50 +0100

    Change <p> to <a>

--- modules/footer.js
+++ modules/footer.js
@@ -1,14 +1,11 @@
 const footer = document.createElement('footer')
-const p = document.createElement('p')
 const source = document.createElement('a')
 
 footer.setAttribute('id', 'footer')
-p.innerText = '\u00A9 2022 github.com/mkj-dev'
 
-source.href = 'https://www.freepik.com/author/valeria-aksakova'
-source.innerText = 'Images by valeria_aksakova on Freepik'
+source.href = 'https://github.com/mkj-dev'
+source.innerText = '\u00A9 2023 github.com/mkj-dev'
 
-footer.appendChild(p)
 footer.appendChild(source)
 
 export { footer }
