5a93fea021b7941e7bf250ebebdc8605013b0da3
mstach0wicz <mikolaj.stachowicz0@gmail.com>
2023-02-09 07:12:49 +0100

    Add target attribute to the footer

--- modules/footer.js
+++ modules/footer.js
@@ -4,6 +4,7 @@ const source = document.createElement('a')
 footer.setAttribute('id', 'footer')
 
 source.href = 'https://github.com/mkj-dev'
+source.setAttribute('target', '_blank')
 source.innerText = '\u00A9 2023 github.com/mkj-dev'
 
 footer.appendChild(source)
